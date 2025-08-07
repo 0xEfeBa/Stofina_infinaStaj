e3aa4d69c9b075ed74bf9ef513e894600a18d741
0xEfeBa <120990958+0xEfeBa@users.noreply.github.com>
2025-08-07 13:11:15 +0300

    feat(stofina): Add orderservice project

---
+++ Stofina/orderservice/src/main/java/com/stofina/app/orderservice/constants/OrderConstants.java
@@ -0,0 +1,16 @@
+package com.stofina.app.orderservice.constants;
+
+public final class OrderConstants {
+
+    private OrderConstants() {
+        // Utility class
+    }
+
+    public static final class Market {
+        public static final String MARKET_OPEN_TIME = "09:30";
+        public static final String MARKET_CLOSE_TIME = "18:00";
+        public static final String TIMEZONE = "Europe/Istanbul";
+        
+        private Market() {}
+    }
+}
