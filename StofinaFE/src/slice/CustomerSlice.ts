d36cb03fc554c2123413f98285850783c6da818c
Ebru-Uslu <ebruebg@hotmail.com>
2025-08-09 01:34:59 +0300

    feat: Autocomplete customer search connected to fetch Customer Services

--- StofinaFE/src/slice/CustomerSlice.ts
+++ StofinaFE/src/slice/CustomerSlice.ts
@@ -1,35 +1,63 @@
+import { CorporateCustomer, IndividualCustomer } from "@/types/customer";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
-import { Customer } from '@/types/customer';
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-
-interface CustomerSlice {
-    isLoading: boolean;
-    customer: Customer | null; // Customer bilgisi null olabilir, çünkü müşteri henüz eklenmemiş olabilir
-    selectedCustomer: Customer | null; // Seçilen müşteri bilgisi null olabilir, çünkü müşteri henüz seçilmemiş olabilir
+interface CustomerState {
+  isLoading: boolean;
+  individualCustomers: IndividualCustomer[] | null; // Customer bilgisi null olabilir, çünkü müşteri henüz eklenmemiş olabilir
+  selectedIndividualCustomer: IndividualCustomer | null; // Seçilen müşteri bilgisi null olabilir, çünkü müşteri henüz seçilmemiş olabilir
+  corporateCustomers: CorporateCustomer[] | null; // Başlangıçta müşteri bilgisi yok
+  selectedCorporateCustomer: CorporateCustomer | null; // Başlangıçta seçilen müşteri bilgisi yok
 }
 
-const initialState: CustomerSlice = {
-    isLoading: false,
-    customer: null, // Başlangıçta müşteri bilgisi yok
-    selectedCustomer: null, // Başlangıçta seçilen müşteri bilgisi yok
+const initialState: CustomerState = {
+  isLoading: false,
+  individualCustomers: null, // Başlangıçta müşteri bilgisi yok
+  selectedIndividualCustomer: null, // Başlangıçta seçilen müşteri bilgisi yok
+  corporateCustomers: null, // Başlangıçta müşteri bilgisi yok
+  selectedCorporateCustomer: null, // Başlangıçta seçilen müşteri bilgisi yok
 };
 
 export const SliceCustomer = createSlice({
-    name: 'customer',
-    initialState,
-    reducers: {
-        setLoading: (state, action: PayloadAction<boolean>) => {
-            state.isLoading = action.payload;
-        },
-        setCustomer: (state, action: PayloadAction<Customer | null>) => {
-            state.customer = action.payload; // Müşteri bilgisi güncelleniyor
-        },
-        setSelectedCustomer: (state, action: PayloadAction<Customer | null>) => {
-            state.selectedCustomer = action.payload; // Seçilen müşteri bilgisi güncelleniyor
-        },
+  name: "customer",
+  initialState,
+  reducers: {
+    // reducerlar state'i günceller
+    setLoading: (state, action: PayloadAction<boolean>) => {
+      state.isLoading = action.payload;
     },
+    setIndividualCustomers: (
+      state,
+      action: PayloadAction<IndividualCustomer[] | null>
+    ) => {
+      state.individualCustomers = action.payload; // Müşteri bilgisi güncelleniyor
+    },
+    setSelectedIndividualCustomer: (
+      state,
+      action: PayloadAction<IndividualCustomer | null>
+    ) => {
+      state.selectedIndividualCustomer = action.payload; // Seçilen müşteri bilgisi güncelleniyor
+    },
+    setCorporateCustomers: (
+      state,
+      action: PayloadAction<CorporateCustomer[] | null>
+    ) => {
+      state.corporateCustomers = action.payload; // Müşteri bilgisi güncelleniyor
+    },
+    setSelectedCorporateCustomer: (
+      state,
+      action: PayloadAction<CorporateCustomer | null>
+    ) => {
+      state.selectedCorporateCustomer = action.payload; // Seçilen müşteri bilgisi güncelleniyor
+    },
+  },
 });
 
-export const { setLoading, setCustomer, setSelectedCustomer } = SliceCustomer.actions;
+export const {
+  setLoading,
+  setIndividualCustomers,
+  setSelectedIndividualCustomer,
+  setCorporateCustomers,
+  setSelectedCorporateCustomer,
+} = SliceCustomer.actions;
 export const { actions, reducer } = SliceCustomer;
 export default SliceCustomer.reducer;
